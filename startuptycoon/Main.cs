1d12a371aae4b34429dcad091a62f47b4f3b235c
Eric <eric.hicks.8989@gmail.com>
2021-12-14 21:14:16 -0800

    Feat: Configured Project
    - Added template for project.

---
+++ startuptycoon/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace startuptycoon
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
